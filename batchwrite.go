0f539ae32a4876065b68b6f6da532f83f11ca69b
Greg <greg@toki.waseda.jp>
2024-01-24 01:06:38 +0900

    batch write: support writing to multiple tables in a single batch

--- batchwrite.go
+++ batchwrite.go
@@ -15,9 +15,14 @@ const maxWriteOps = 25
 // BatchWrite is a BatchWriteItem operation.
 type BatchWrite struct {
 	batch Batch
-	ops   []*dynamodb.WriteRequest
+	ops   []batchWrite
 	err   error
 	cc    *ConsumedCapacity
+}
+
+type batchWrite struct {
+	table string
+	op    *dynamodb.WriteRequest
 }
 
 // Write creates a new batch write request, to which
@@ -29,29 +34,63 @@ func (b Batch) Write() *BatchWrite {
 	}
 }
 
-// Put adds put operations for items to this batch.
+// Put adds put operations for items to this batch using the default table.
 func (bw *BatchWrite) Put(items ...interface{}) *BatchWrite {
+	return bw.PutIn(bw.batch.table, items...)
+}
+
+// PutIn adds put operations for items to this batch using the given table.
+// This can be useful for writing to multiple different tables.
+func (bw *BatchWrite) PutIn(table Table, items ...interface{}) *BatchWrite {
+	name := table.Name()
 	for _, item := range items {
 		encoded, err := marshalItem(item)
 		bw.setError(err)
-		bw.ops = append(bw.ops, &dynamodb.WriteRequest{PutRequest: &dynamodb.PutRequest{
-			Item: encoded,
-		}})
+		bw.ops = append(bw.ops, batchWrite{
+			table: name,
+			op: &dynamodb.WriteRequest{PutRequest: &dynamodb.PutRequest{
+				Item: encoded,
+			}},
+		})
 	}
 	return bw
 }
 
-// Delete adds delete operations for the given keys to this batch.
+// Delete adds delete operations for the given keys to this batch, using the default table.
 func (bw *BatchWrite) Delete(keys ...Keyed) *BatchWrite {
+	return bw.deleteIn(bw.batch.table, bw.batch.hashKey, bw.batch.rangeKey, keys...)
+}
+
+// DeleteIn adds delete operations for the given keys to this batch, using the given table.
+// hashKey must be the name of the primary key hash (partition) attribute.
+// This function is for tables with a hash key (partition key) only.
+// For tables including a range key (sort key) primary key, use [BatchWrite.DeleteInRange] instead.
+func (bw *BatchWrite) DeleteIn(table Table, hashKey string, keys ...Keyed) *BatchWrite {
+	return bw.deleteIn(table, hashKey, "", keys...)
+}
+
+// DeleteInRange adds delete operations for the given keys to this batch, using the given table.
+// hashKey must be the name of the primary key hash (parition) attribute, rangeKey must be the name of the primary key range (sort) attribute.
+// This function is for tables with a hash key (partition key) and range key (sort key).
+// For tables without a range key primary key, use [BatchWrite.DeleteIn] instead.
+func (bw *BatchWrite) DeleteInRange(table Table, hashKey, rangeKey string, keys ...Keyed) *BatchWrite {
+	return bw.deleteIn(table, hashKey, rangeKey, keys...)
+}
+
+func (bw *BatchWrite) deleteIn(table Table, hashKey, rangeKey string, keys ...Keyed) *BatchWrite {
+	name := table.Name()
 	for _, key := range keys {
-		del := bw.batch.table.Delete(bw.batch.hashKey, key.HashKey())
-		if rk := key.RangeKey(); bw.batch.rangeKey != "" && rk != nil {
-			del.Range(bw.batch.rangeKey, rk)
+		del := table.Delete(hashKey, key.HashKey())
+		if rk := key.RangeKey(); rangeKey != "" && rk != nil {
+			del.Range(rangeKey, rk)
 			bw.setError(del.err)
 		}
-		bw.ops = append(bw.ops, &dynamodb.WriteRequest{DeleteRequest: &dynamodb.DeleteRequest{
-			Key: del.key(),
-		}})
+		bw.ops = append(bw.ops, batchWrite{
+			table: name,
+			op: &dynamodb.WriteRequest{DeleteRequest: &dynamodb.DeleteRequest{
+				Key: del.key(),
+			}},
+		})
 	}
 	return bw
 }
@@ -108,12 +147,21 @@ func (bw *BatchWrite) RunWithContext(ctx context.Context) (wrote int, err error)
 				}
 			}
 
-			unprocessed := res.UnprocessedItems[bw.batch.table.Name()]
-			wrote += len(ops) - len(unprocessed)
-			if len(unprocessed) == 0 {
+			wrote += len(ops)
+			if len(res.UnprocessedItems) == 0 {
 				break
 			}
-			ops = unprocessed
+
+			ops = ops[:0]
+			for tableName, unprocessed := range res.UnprocessedItems {
+				wrote -= len(unprocessed)
+				for _, op := range unprocessed {
+					ops = append(ops, batchWrite{
+						table: tableName,
+						op:    op,
+					})
+				}
+			}
 
 			// need to sleep when re-requesting, per spec
 			if err := aws.SleepWithContext(ctx, boff.NextBackOff()); err != nil {
@@ -126,11 +174,13 @@ func (bw *BatchWrite) RunWithContext(ctx context.Context) (wrote int, err error)
 	return wrote, nil
 }
 
-func (bw *BatchWrite) input(ops []*dynamodb.WriteRequest) *dynamodb.BatchWriteItemInput {
+func (bw *BatchWrite) input(ops []batchWrite) *dynamodb.BatchWriteItemInput {
+	items := make(map[string][]*dynamodb.WriteRequest)
+	for _, op := range ops {
+		items[op.table] = append(items[op.table], op.op)
+	}
 	input := &dynamodb.BatchWriteItemInput{
-		RequestItems: map[string][]*dynamodb.WriteRequest{
-			bw.batch.table.Name(): ops,
-		},
+		RequestItems: items,
 	}
 	if bw.cc != nil {
 		input.ReturnConsumedCapacity = aws.String(dynamodb.ReturnConsumedCapacityIndexes)

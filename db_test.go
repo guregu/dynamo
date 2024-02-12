6c46b7798b8b32ad0d977bf7619b729eb802c48d
Greg <greg@toki.waseda.jp>
2024-02-12 13:24:02 +0900

    add github actions ci & auto-create test table

--- db_test.go
+++ db_test.go
@@ -1,11 +1,16 @@
 package dynamo
 
 import (
+	"errors"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
@@ -19,10 +24,19 @@ var dummyCreds = credentials.NewStaticCredentials("dummy", "dummy", "")
 
 const offlineSkipMsg = "DYNAMO_TEST_REGION not set"
 
-func init() {
-	// os.Setenv("DYNAMO_TEST_REGION", "us-west-2")
+// widget is the data structure used for integration tests
+type widget struct {
+	UserID int       `dynamo:",hash"`
+	Time   time.Time `dynamo:",range" index:"Msg-Time-index,range"`
+	Msg    string    `index:"Msg-Time-index,hash"`
+	Count  int
+	Meta   map[string]string
+	StrPtr *string `dynamo:",allowempty"`
+}
+
+func TestMain(m *testing.M) {
+	var endpoint *string
 	if region := os.Getenv("DYNAMO_TEST_REGION"); region != "" {
-		var endpoint *string
 		if dte := os.Getenv("DYNAMO_TEST_ENDPOINT"); dte != "" {
 			endpoint = aws.String(dte)
 		}
@@ -33,18 +47,42 @@ func init() {
 		})
 	}
 	if table := os.Getenv("DYNAMO_TEST_TABLE"); table != "" {
+		// Test-% --> Test-1707708680863
+		table = strings.ReplaceAll(table, "%", strconv.FormatInt(time.Now().UnixMilli(), 10))
 		testTable = table
+	}
+
+	var created []Table
+	if testDB != nil {
+		table := testDB.Table(testTable)
+		log.Println("Checking test table:", testTable)
+		_, err := table.Describe().Run()
+		switch {
+		case isTableNotExistsErr(err) && endpoint != nil:
+			log.Println("Creating test table:", testTable)
+			if err := testDB.CreateTable(testTable, widget{}).Run(); err != nil {
+				panic(err)
+			}
+			created = append(created, testDB.Table(testTable))
+		case err != nil:
+			panic(err)
+		}
+	}
+
+	code := m.Run()
+	defer os.Exit(code)
+
+	for _, table := range created {
+		log.Println("Deleting test table:", table.Name())
+		if err := table.DeleteTable().Run(); err != nil {
+			log.Println("Error deleting test table:", table.Name(), err)
+		}
 	}
 }
 
-// widget is the data structure used for integration tests
-type widget struct {
-	UserID int       `dynamo:",hash"`
-	Time   time.Time `dynamo:",range"`
-	Msg    string
-	Count  int
-	Meta   map[string]string
-	StrPtr *string `dynamo:",allowempty"`
+func isTableNotExistsErr(err error) bool {
+	var ae awserr.Error
+	return errors.As(err, &ae) && ae.Code() == "ResourceNotFoundException"
 }
 
 func TestListTables(t *testing.T) {
